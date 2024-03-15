e4316b7ca7bdd862f605f2bdfac904f1d072f8de
K\K <kwonabraham@naver.com>
2024-03-15 23:22:22 +0900

    DTO 생성

---
+++ src/main/java/com/study/erum/fileupload/MyfileDTO.java
@@ -0,0 +1,18 @@
+package com.study.erum.fileupload;
+
+public class MyfileDTO {
+
+	private String idx;
+	private String title;
+	private String cate;
+	private String ofile;
+	private String sfile;
+	private String postdate;
+	
+	public String getIdx() {
+		return idx;
+	}
+	public void setIdx(String idx) {
+		this.idx = idx;
+	}
+}

4d39c5fd71c12d38d9fb95ce1fb4b550ba7970d1
K\K <kwonabraham@naver.com>
2024-03-16 23:16:17 +0900

    DAO 작성

---
+++ src/main/java/com/study/erum/fileupload/MyFileDAO.java
@@ -0,0 +1,26 @@
+package com.study.erum.fileupload;
+
+public class MyFileDAO extends DBConnPool{
+
+	public int inserFile(MyFileDTO dto) {
+		
+		int applyResult = 0;
+		
+		try {
+			String query = "insert into myfile (idx, title, cate, ofile, sfile) values (seq_board_num.nextval, ?, ?, ?, ?, ?)";
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, dto.getTitle());
+			pstmt.setString(2, dto.getCate());
+			pstmt.setString(3, dto.getOfile());
+			pstmt.setString(4, dto.getSfile());
+			
+			applyResult = pstmt.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("Insert 중 예외 발생");
+			e.printStackTrace();
+		}
+		
+		return applyResult;
+	}
+}

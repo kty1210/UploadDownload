5af34fa162e3156f2ea7e4b6274fb54ba77e7e7e
Kwon <kwonabraham@naver.com>
2024-03-23 22:01:02 +0900

    DAO 생성

---
+++ src/main/java/fileupload/MyFileDAO.java
@@ -0,0 +1,35 @@
+package fileupload;
+
+import java.util.List;
+import java.util.Vector;
+
+import common.DBConnPool;
+
+public class MyFileDAO extends DBConnPool { 
+	// 새로운 게시물을 입력합니다.
+    public int insertFile(MyFileDTO dto) {
+        int applyResult = 0;
+        try {
+            String query = "INSERT INTO myfile ( "
+                + " idx, title, cate, ofile, sfile) "
+                + " VALUES ( "
+                + " seq_board_num.nextval, ?, ?, ?, ?)";
+                        
+            psmt = con.prepareStatement(query);
+            psmt.setString(1, dto.getTitle());
+            psmt.setString(2, dto.getCate());
+            psmt.setString(3, dto.getOfile());
+            psmt.setString(4, dto.getSfile());
+        
+            applyResult = psmt.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println("INSERT 중 예외 발생");
+            e.printStackTrace();
+        }        
+        return applyResult;
+    }
+
+    
+}
+

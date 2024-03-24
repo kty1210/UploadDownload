e301493c2c066b35bdb8f8a337814781599116ac
Kwon <kwonabraham@naver.com>
2024-03-24 12:55:49 +0900

    목록 가져오는 메소드 추가

--- src/main/java/fileupload/MyFileDAO.java
+++ src/main/java/fileupload/MyFileDAO.java
@@ -30,6 +30,32 @@ public class MyFileDAO extends DBConnPool {
         return applyResult;
     }
 
-    
+    public List<MyFileDTO> myFileList() {
+        List<MyFileDTO> fileList = new Vector<MyFileDTO>();
+
+        String query = "SELECT * FROM myfile ORDER BY idx DESC";
+        try {
+            stmt = con.createStatement();   
+            rs = stmt.executeQuery(query);   
+
+            while (rs.next()) {   
+                MyFileDTO dto = new MyFileDTO();
+                dto.setIdx(rs.getString(1));
+                dto.setTitle(rs.getString(2));
+                dto.setCate(rs.getString(3));
+                dto.setOfile(rs.getString(4));
+                dto.setSfile(rs.getString(5));
+                dto.setPostdate(rs.getString(6));
+                
+                fileList.add(dto);  
+            }
+        }
+        catch (Exception e) {
+            System.out.println("SELECT 시 예외 발생");
+            e.printStackTrace();
+        }        
+        
+        return fileList;  
+    }
 }
 

e5116697171b23226efc33d2ebad918d3a84a33d
Kwon <kwonabraham@naver.com>
2024-03-24 23:47:50 +0900

    FileUtil에 멀티 Upload 메소드 작성

--- src/main/java/fileupload/FileUtil.java
+++ src/main/java/fileupload/FileUtil.java
@@ -3,6 +3,8 @@ package fileupload;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -59,4 +61,39 @@ public class FileUtil {
 	    return newFileName;
 	}
 	
+	
+	//multiple 속성 추가로 2개 이상의 파일 업로드
+		public static ArrayList<String> multipleFile(HttpServletRequest req, String sDirectory)
+				throws ServletException, IOException {
+			//파일명 저장을 위한 컬렉션 생성
+			ArrayList<String> listFileName = new ArrayList<>();
+		
+			//Part 객체를 통해 서버로 전송된 파일명 읽어오기 
+			Collection<Part> parts = req.getParts();
+			for(Part part : parts) {
+				//파일이 아니라면 업로드의 대상이 아니므로 무시
+				if(!part.getName().equals("ofile"))
+					continue;	
+				
+				//Part 객체의 헤더값 중 content-disposition 읽어오기 
+		        String partHeader = part.getHeader("content-disposition");
+		        //출력결과 => form-data; name="attachedFile"; filename="파일명.jpg"
+		        System.out.println("partHeader="+ partHeader);
+		        
+		        //헤더값에서 파일명 잘라내기
+		        String[] phArr = partHeader.split("filename=");
+		        String originalFileName = phArr[1].trim().replace("\"", "");
+				
+				//전송된 파일이 있다면 디렉토리에 저장
+				if (!originalFileName.isEmpty()) {				
+					part.write(sDirectory+ File.separator +originalFileName);
+				}
+				
+				//컬렉션에 추가
+				listFileName.add(originalFileName);
+			}
+		
+			//원본 파일명 반환
+			return listFileName;			
+		}
 }
